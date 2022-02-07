431d7b2ed5d34a32b11d06d88a9178ab2b3951a5
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-02-07 15:09:58 -0800

    socket.io install and setup connection

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var express = require("express")
 var bodyParser = require("body-parser")
+var io = require("socket.io")
 
-var app = express()
+var app = express() // run node on express backend (client -> server only) 
+/* socket.io needs to tie in with Express, and the game plan is that we'll create a regular HTTP server with Node that will then share with Express and Socket.IO. 
+First let's set up that HTTP server: 
+- Add var http require then we'll use the HTTP library from Node and we'll call .Server on the require itself and pass in our Express app
+- Then let's create io and set it to require socket.io and we'll pass in reference to http*/
+var http = require("http").Server(app)
+var io = require("socket.io")(http)
 
 // tell express to use static content
 app.use(express.static(__dirname))
 app.use(bodyParser.json()) // expect json 
 app.use(bodyParser.urlencoded({extended: false})) // browser data
+
 
 var messages = [
     {name: "Tim", message: "Hi"}, 
@@ -23,8 +31,16 @@ app.post('/messages', (req, res) => {
     messages.push(req.body) // now change to put it to the messages array
     res.sendStatus(200)
 })
+
+/* add io.on and we'll check for the connection event, and we'll supply a function that takes in a socket. And for now, let's console.log a user connected, we can see that a Socket.IO connection has successfully been made from the browser since we're getting a message in the connection event and we can see a user connected in our Console. */    
+io.on("connection", (socket) => {
+    console.log("a user connected")
+})
+
 //app.listen(3000)
 // dynamic reaing the port
-var server = app.listen(3000, () => {
+/* socket.io implemntation: need to use the new http server to listen
+var server = app.listen(3000, () => {*/
+var server = http.listen(3000, () => {    
     console.log(`Express server is listening on port ${server.address().port}`)
 })
