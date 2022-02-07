032d926c7bbba6175773d62ac1df05adeb2083ec
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-02-06 22:00:04 -0800

    Add get and post functions

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 var express = require("express")
+var bodyParser = require("body-parser")
 
 var app = express()
 
 // tell express to use static content
 app.use(express.static(__dirname))
+app.use(bodyParser.json()) // expect json 
+app.use(bodyParser.urlencoded({extended: false})) // browser data
 
+var messages = [
+    {name: "Tim", message: "Hi"}, 
+    {name: "Jane", message: "Hello"}
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    // console.log(req.body) - this should log the json data in the console from postman request for debugging
+    //console.log(req.body)
+    messages.push(req.body) // now change to put it to the messages array
+    res.sendStatus(200)
+})
 //app.listen(3000)
-
+// dynamic reaing the port
 var server = app.listen(3000, () => {
     console.log(`Express server is listening on port ${server.address().port}`)
 })
