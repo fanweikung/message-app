e1889066b78466f46c78281ab2f858bd599664cb
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-02-08 11:51:14 -0800

    Connect via Mongoose to post and get messages

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require("express")
 var bodyParser = require("body-parser")
 var io = require("socket.io")
-//var mongoose = require("mongoose")
-const { MongoClient } = require('mongodb')
+var mongoose = require("mongoose")
+const { sendStatus } = require("express/lib/response")
+//const { MongoClient } = require('mongodb')
 
 var app = express() // run node on express backend (client -> server only) 
 /* socket.io needs to tie in with Express, and the game plan is that we'll create a regular HTTP server with Node that will then share with Express and Socket.IO. 
@@ -12,53 +13,68 @@ First let's set up that HTTP server:
 var http = require("http").Server(app)
 var io = require("socket.io")(http)
 /* 
-mongoose connection example:
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://user:<password>@learning-node.qallf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});*/
+mongo connection
 const uri = "mongodb+srv://user:user@learning-node.qallf.mongodb.net/"
-//"mongodb+srv://user:user@learning-node.qallf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+*/
 
-
+/* mongoose connection */
+const dbUrl = "mongodb+srv://user:user@learning-node.qallf.mongodb.net/"
 
 // tell express to use static content
 app.use(express.static(__dirname))
 app.use(bodyParser.json()) // expect json 
 app.use(bodyParser.urlencoded({extended: false})) // browser data
 
+// object data model
+var Message = mongoose.model('message', {
+    name: String,
+    message: String
+})
 
+/* no longer need the hard coded messages for get 
+    TODO: why will it crash when removing the messages array??? */    
 var messages = [
     {name: "Tim", message: "Hi"}, 
     {name: "Jane", message: "Hello"}
 ]
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    /* instead of send the hard coded messages, send from mongoDB with no filter {<field>: 'value'} applied
+    res.send(messages)*/
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
 })
 
 app.post('/messages', (req, res) => {
     // console.log(req.body) - this should log the json data in the console from postman request for debugging
     //console.log(req.body)
+
+    // instance of ODM
+    var message = new Message(req.body)
+    message.save((err) => {
+        if (err)
+            sendStatus(500)
+    
+        // if saved to mongodb
+        io.emit("message", req.body)
+        messages.push(req.body)
+        res.sendStatus(200)    
+
+
+    })
+    /*
     io.emit("message", req.body)
-    messages.push(req.body) // now change to put it to the messages array
-    res.sendStatus(200)
+    messages.push(req.body) now change to put it to the messages array
+    res.sendStatus(200) */
 })
 
 /* add io.on and we'll check for the connection event, and we'll supply a function that takes in a socket. And for now, let's console.log a user connected, we can see that a Socket.IO connection has successfully been made from the browser since we're getting a message in the connection event and we can see a user connected in our Console. */    
 io.on("connection", (socket) => {
     console.log("a user connected")
 })
-/*
-mongoose.connect(dbUrl, (err) => {
-    console.log(`console db connection`, err)
-})*/
 
-/* connect to mongoDB directly instead of using mongoose */
+/* connect to mongoDB directly instead of using mongoose 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
@@ -66,10 +82,16 @@ client.connect(err => {
   if (err) throw {err}
   console.log('mongo db connection')
   client.close();
+}) */
+
+/* connect to mongoDB using mongoose */
+mongoose.connect(dbUrl, err => {
+    if (err){console.error(`$err`)}
+    console.log('connect to db using mongoose')
 })
 
 //app.listen(3000)
-// dynamic reaing the port
+// dynamic reading the port
 /* socket.io implemntation: need to use the new http server to listen
 var server = app.listen(3000, () => {*/
 var server = http.listen(3000, () => {    
