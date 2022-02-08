9dbf9b5716f166c0ff3b89a187b322d611b57874
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-02-08 06:58:46 -0800

    Connect with MongoDB directly instead of using Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express")
 var bodyParser = require("body-parser")
 var io = require("socket.io")
+//var mongoose = require("mongoose")
+const { MongoClient } = require('mongodb')
 
 var app = express() // run node on express backend (client -> server only) 
 /* socket.io needs to tie in with Express, and the game plan is that we'll create a regular HTTP server with Node that will then share with Express and Socket.IO. 
@@ -9,6 +11,20 @@ First let's set up that HTTP server:
 - Then let's create io and set it to require socket.io and we'll pass in reference to http*/
 var http = require("http").Server(app)
 var io = require("socket.io")(http)
+/* 
+mongoose connection example:
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://user:<password>@learning-node.qallf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});*/
+const uri = "mongodb+srv://user:user@learning-node.qallf.mongodb.net/"
+//"mongodb+srv://user:user@learning-node.qallf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+
 
 // tell express to use static content
 app.use(express.static(__dirname))
@@ -28,14 +44,28 @@ app.get('/messages', (req, res) => {
 app.post('/messages', (req, res) => {
     // console.log(req.body) - this should log the json data in the console from postman request for debugging
     //console.log(req.body)
-    messages.push(req.body) // now change to put it to the messages array
     io.emit("message", req.body)
+    messages.push(req.body) // now change to put it to the messages array
     res.sendStatus(200)
 })
 
 /* add io.on and we'll check for the connection event, and we'll supply a function that takes in a socket. And for now, let's console.log a user connected, we can see that a Socket.IO connection has successfully been made from the browser since we're getting a message in the connection event and we can see a user connected in our Console. */    
 io.on("connection", (socket) => {
     console.log("a user connected")
+})
+/*
+mongoose.connect(dbUrl, (err) => {
+    console.log(`console db connection`, err)
+})*/
+
+/* connect to mongoDB directly instead of using mongoose */
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  if (err) throw {err}
+  console.log('mongo db connection')
+  client.close();
 })
 
 //app.listen(3000)
