15167e46f1b91601eddaf93b610f3248afbaad32
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-02-09 11:53:46 -0800

    ex: convert error handling to async-await

--- examples/async-await-ex.js
+++ examples/async-await-ex.js
@@ -1,3 +1,6 @@
+const { request } = require("express")
+
+// 1 convert a function
 MyFunction(){
     GetMessages((list) => {
         console.log(list)
@@ -7,4 +10,19 @@ MyFunction(){
 async MyFunction(){
     let list = await GetMessages()
     console.log(list)
+}
+
+// 2 Convert error handling
+MyFunction(){
+    request((result, err) => console.log(result, err))
+}
+
+async MyFunction(){
+    try {
+        let result = await request()
+        console.log(result)
+    } catch (error) {
+        console.log(error)
+    }
+
 }
