ea40bc4eb0a5244f7cb58a54eccd5d4292dc5a9b
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-02-09 13:54:31 -0800

    Comment out throw some error test

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post('/messages', async (req, res) => {
     //console.log(req.body)
     //
     try {
-        throw 'some error'
+        //throw 'some error'
         var message = new Message(req.body)
 
         var savedMessage = await message.save()
