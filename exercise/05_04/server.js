2cd87dda842db797fbae86a5143ceed3268df643
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-05-06 17:28:40 -0700

    05_04 Create a post message
    1. Use postman to post a message; verify status = 200
    2. npm install bodyParser as middleware
    3. Add postMessage function to send entered name and message at send
    4. The posted message can be seen by two clients via browser refresh

---
+++ exercise/05_04/server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); // needed when sent from browser; console.log would have shown an empty array
+
+var messages = [
+  { name: "Tim", message: "Hi" },
+  { name: "Jane", message: "Hello" },
+];
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.post("/messages", (req, res) => {
+  console.log(req.body);
+  messages.push(req.body); // posted from postman; append to the messages arrary
+  res.sendStatus(200);
+});
+
+var server = app.listen(3000, () => {
+  console.log("server is listening on port", server.address().port);
+});
