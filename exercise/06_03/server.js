5d9fb628ff8294a53332e6f57788174797c92609
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-05-08 07:07:56 -0700

    06_04 07_01 07_02 Mongoose ODM and Promise
    06_04 Use Mongoose ODM to Save and Get Data
    07_01 07_02 Use Promise to improve nested callback

--- exercise/06_03/server.js
+++ exercise/06_03/server.js
@@ -9,22 +9,74 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// 07_02 Tell mongoose to use default ES6 Promise Library
+mongoose.Promise = Promise;
+
 var dbUrl =
   "mongodb+srv://user:user@learning-node.qallf.mongodb.net/?retryWrites=true&w=majority";
 
-var messages = [
-  { name: "Tim", message: "Hi" },
-  { name: "Jane", message: "Hello" },
-];
+// 06_04 Use Mongoose ODM to Save and Get Data
+// 07_01 07_02
+// - Use Promise and Async Await to improve nested callback
+// - Mongoose not loger accept call back at connect & save
+// var messages = [
+//   { name: "Tim", message: "Hi" },
+//   { name: "Jane", message: "Hello" },
+// ];
+//
+// app.get("/messages", (req, res) => {
+//   res.send(messages);
+// });
 
-app.get("/messages", (req, res) => {
+// Capital M indiciates a model
+var Message = mongoose.model("Message", {
+  name: String,
+  message: String,
+});
+
+// use async await
+app.get("/messages", async (req, res) => {
+  var messages = await Message.find({});
   res.send(messages);
 });
 
+// Alternatively, Use then and chanted functions
+// app.get("/messages", (req, res) => {
+//   var messages;
+//   Message.find({}).then((messages) => {
+//     res.send(messages);
+//   });
+// });
+
+// app.post("/messages", (req, res) => {
+//   messages.push(req.body);
+//   io.emit("message", req.body);
+//   res.sendStatus(200);
+// });
+//
+// Use then and chanted functions
+// - TODO TypeError: Message.remove is not a function
 app.post("/messages", (req, res) => {
-  messages.push(req.body);
-  io.emit("message", req.body);
-  res.sendStatus(200);
+  var message = new Message(req.body);
+
+  message
+    .save()
+    .then(() => {
+      console.log("saved");
+      return Message.findOne({ message: "badword" });
+    })
+    .then((censored) => {
+      if (censored) {
+        console.log("censored words found", censored);
+        return Message.remove({ _id: censored.id });
+      }
+      io.emit("message", req.body);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+      return console.error(err);
+    });
 });
 
 io.on("connection", (socket) => {
