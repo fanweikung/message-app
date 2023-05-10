fc28ee70ab93e85eb3e00b726e63335a0a738015
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-05-10 16:40:59 -0700

    Challenges - Convert to async/await w try catch

--- exercise/06_03/Challenges.js
+++ exercise/06_03/Challenges.js
@@ -16,3 +16,19 @@ async MyFunction(){
     let list = await GetMessages(list);
     console.log(list);
 };
+
+// 2.
+MyFunction(){
+    request((result, err) => console.log(result, err))
+}
+
+// async await w try catch
+async MyFunction(){
+    try {
+        let result = await request()
+        console.log(result)
+    } catch (err) {
+        console.log(err)
+    }
+    
+}
